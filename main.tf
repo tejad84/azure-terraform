b4a582a5d0ae3405fbe04a46bb881be0f8f7fc68
dharmateja <tejad76@gmail.com>
2022-11-23 23:06:08 +0530

    add varable file

--- main.tf
+++ main.tf
@@ -1,10 +1,15 @@
 module "resource_group" {
-source = "./resource_group"
+  source   = "./resource_group"
   location = "West Europe"
   name     = "test-v-kt"
 
 }
 
 provider "azurerm" {
-  features {}  
+  client_id       = var.client_id
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_secret   = var.client_secret
+
+  features {}
 }
